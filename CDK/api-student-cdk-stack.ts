3b0b3922cd59e72553e8361b3eb4315af4a5f3eb
diegorivadeneyra <55153225+diegorivadeneyra@users.noreply.github.com>
2025-09-14 13:17:57 -0500

    Update api-student-cdk-stack.ts

--- CDK/api-student-cdk-stack.ts
+++ CDK/api-student-cdk-stack.ts
@@ -1,46 +1,61 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as logs from 'aws-cdk-lib/aws-logs';
 
 export class ApiStudentCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Buscar la VPC por defecto
-    const vpc = ec2.Vpc.fromLookup(this, 'DefaultVPC', { isDefault: true });
+    const vpc = ec2.Vpc.fromLookup(this, 'DefaultVPC', {
+      isDefault: true,
+    });
 
-    // Crear el clúster ECS
-    const cluster = new ecs.Cluster(this, 'ApiCluster', { vpc });
+    const sg = new ec2.SecurityGroup(this, 'CrudSecurityGroup', {
+      vpc,
+      description: 'Allow inbound traffic on port 8000',
+      allowAllOutbound: true,
+    });
 
-    // Reutilizar el rol LabRole para evitar errores de permisos
-    const labRole = iam.Role.fromRoleArn(this, 'LabRole',
-      'arn:aws:iam::472685703742:role/LabRole'
-    );
+    sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(8000), 'Allow HTTP on port 8000');
 
-    // Definición de tarea Fargate con imagen desde ECR
-    const taskDefinition = new ecs.FargateTaskDefinition(this, 'ApiTaskDef', {
+    const cluster = new ecs.Cluster(this, 'CrudCluster', {
+      vpc,
+    });
+
+    const labRole = iam.Role.fromRoleArn(this, 'LabRole', 'arn:aws:iam::472685703742:role/LabRole', {
+      mutable: false,
+    });
+
+    const logGroup = new logs.LogGroup(this, 'ApiLogsGroup', {
+      retention: logs.RetentionDays.ONE_WEEK,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'CrudTaskDef', {
+      memoryLimitMiB: 512,
+      cpu: 256,
       executionRole: labRole,
       taskRole: labRole,
     });
 
-    const container = taskDefinition.addContainer('ApiContainer', {
+    taskDefinition.addContainer('CrudContainer', {
       image: ecs.ContainerImage.fromRegistry(
         '472685703742.dkr.ecr.us-east-1.amazonaws.com/api-students:latest'
       ),
-      memoryLimitMiB: 512,
-      cpu: 256,
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'ApiLogs', logGroup }),
+      portMappings: [{ containerPort: 8000 }],
     });
 
-    container.addPortMappings({ containerPort: 8000 });
-
-    // Servicio Fargate con IP pública
-    new ecs.FargateService(this, 'ApiService', {
+    new ecs.FargateService(this, 'CrudService', {
       cluster,
       taskDefinition,
       desiredCount: 1,
       assignPublicIp: true,
+      securityGroups: [sg],
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
     });
   }
 }
